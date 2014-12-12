593270665ce2c24ebbc6b746b9b4994806cc031a
Giovanni Della Lunga <giovanni.dellalunga@gmail.com>
2014-12-12 22:44:37 +0100

    dev

---
+++ py/lingua_francese/exceptions_feminin.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+except_fem_end_e = { 'prince'   :   'princesse'
+                    ,'poète'    :   'poétesse'
+                    ,'maitre'   :   'maitresse'
+                    ,'comte'    :   'comtesse'
+                    ,'tigre'    :   'tigresse'
+                    ,'diable'   :   'diablesse'
+                    }
+
+except_fem_end_teur =   { 'chanteur'    :   'chanteuse'
+                         ,'acheteur'    :   'acheteuse'
+                         ,'menteur'     :   'menteuse'
+                     }
