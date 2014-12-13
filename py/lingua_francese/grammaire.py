79ce88873b343d39ebde7d872f5ad8b5a933d09c
Giovanni Della Lunga <giovanni.dellalunga@gmail.com>
2014-12-13 22:37:13 +0100

    dev

--- py/lingua_francese/grammaire.py
+++ py/lingua_francese/grammaire.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 from exceptions_pluriel import *
 from exceptions_feminin import *
 
@@ -28,16 +30,53 @@ def pluriel(token):
     return unicode(token + 's')    
 #_______________________________________________________________________________________________________________________________
 #
-def feminin(token):
+def feminin_nom(token):
     
-    pass
+    # controllo terminazione in -e
+    if token[-1] == 'e':
+        if token in except_fem_nom_end_e.keys():
+            return except_fem_nom_end_e[token]
+        return token
+    
+    # nomi terminanti in -er
+    if token[-2:] == 'er':
+        return token[:-2] + 'ère'
+    
+    # nomi terminanti in -an, -en, -on, -el, -et
+    if token[-2:] in ['an','en','on','el','et']:
+        ultima_consonante = token[-1]
+        return token + ultima_consonante + 'e'
+    
+    # nomi terminanti in -f
+    if token[-1]=='f':
+        return token[:-1] + 've'
+    
+    # nomi terminanti in -x
+    if token[-1]=='x':
+        return token[:-1] + 'se'
+    
+    # attenzione all'ordine dei prossimi due controlli
+    # l'ordine non deve essere invertito
+    # nomi terminanti in -teur
+    if token[-4:]=='teur':
+        if token in except_fem_nom_end_teur.keys():
+            return except_fem_nom_end_teur[token]
+        return token[:-4] + 'trice'
+    
+    # nomi terminanti in -eur
+    if token[-3:] == 'eur':
+        return token[:-3] + 'euse'
+    
+    return token + 'e'
+    
 #_______________________________________________________________________________________________________________________________
 #
 if __name__ == "__main__":
     while 1:
-        token  = raw_input("Inserire il termine al singolare > ")
+        #token  = raw_input("Inserire il termine al singolare > ")
+        token  = raw_input("Inserire il termine al maschile > ")
         if token == '0':
             break
-        print pluriel(unicode(token))
+        print feminin_nom(unicode(token))
         
     print 'programma terminato'    
