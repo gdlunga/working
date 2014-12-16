8bb62a2df6eb1aab9142da8e5c2685aad7191c9d
gdlunga <giovanni.dellalunga@gmail.com>
2014-12-16 17:28:07 +0100

    dev

---
+++ cs/MTServer/TestLib/Sample.cs
@@ -0,0 +1,49 @@
+﻿/*
+using RGiesecke.DllExport;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLib
+{
+    [ComVisible(true), ClassInterface(ClassInterfaceType.AutoDual)]
+    public class TestClass
+    {
+        public string Text
+        {
+            [return: MarshalAs(UnmanagedType.BStr)]
+            get;
+            [param: MarshalAs(UnmanagedType.BStr)]
+            set;
+        }
+
+        public int Numbers
+        {
+            [return: MarshalAs(UnmanagedType.SysInt)]
+            get;
+            [param: MarshalAs(UnmanagedType.SysInt)]
+            set;
+        }
+
+        [return: MarshalAs(UnmanagedType.SysInt)]
+        public int GetRandomNumber()
+        {
+            Random x = new Random();
+            return x.Next(100);
+        }
+    }
+
+    static class UnmanagedExports
+    {
+        [DllExport]
+        [return: MarshalAs(UnmanagedType.IDispatch)]
+        static Object CreateTestClass()
+        {
+            return new TestClass();
+        }
+    }
+}
+*/
