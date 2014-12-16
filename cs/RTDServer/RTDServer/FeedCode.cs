f96a0ec2f45bc1733cc16a13516ac5c2bacde4d1
gdlunga <giovanni.dellalunga@gmail.com>
2014-12-16 17:30:08 +0100

    dev

---
+++ cs/RTDServer/RTDServer/FeedCode.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RTDServer
+{
+    public class FeedCode
+    {
+        public string code;
+        public string field;
+        public string datatable;
+    }
+
+    public class FeedCode2 : FeedCode
+    {
+        public int freq;
+    }
+}
