cf4f55ec06cf9f989e2998c004d990d48ab2a36a
gdlunga <giovanni.dellalunga@gmail.com>
2014-12-19 17:11:30 +0100

    dev

--- cs/MTServer/MTServer/TestClass.cs
+++ cs/MTServer/MTServer/TestClass.cs
@@ -12,6 +12,21 @@ using Npgsql;
 
 namespace TestLib
 {
+    public class myToken
+    {
+        private String feedCode;
+        private String feedValue;
+
+        public myToken()
+        {
+        }
+        public myToken(String feedCode, String feedValue)
+        {
+            this.feedCode = feedCode;
+            this.feedValue = feedValue;
+        }
+    }
+
     [ComVisible(true), ClassInterface(ClassInterfaceType.AutoDual)]
     public class TestClass
     {
@@ -33,6 +48,10 @@ namespace TestLib
         public String TestUpdate([MarshalAs(UnmanagedType.SafeArray)]ref string[] feedCode
                                 ,[MarshalAs(UnmanagedType.SafeArray)]ref string[] feedValues)
         {
+
+            Int32 blockSize      = 10;
+
+            StringBuilder bulkInsertCmd = new StringBuilder();
             StringBuilder updateCommand = new StringBuilder();
             String tableName = "data_double";
 
@@ -51,6 +70,8 @@ namespace TestLib
                     NpgsqlCommand pgsqlCommand = new NpgsqlCommand();
                     pgsqlCommand.Connection = pgsqlConnection;
 
+                    bulkInsertCmd.Length = 0;
+                    int updateCount = 0;
                     for (int i = 0; i < feedCode.Length; i++)
                     {
                         thisFeedCode = feedCode[i];
@@ -67,10 +88,18 @@ namespace TestLib
                         updateCommand.Append("')");
 
                         string cmd = updateCommand.ToString();
-
-                        pgsqlCommand.CommandText = cmd;
-                        pgsqlCommand.ExecuteNonQuery();
-                        pgsqlCommand.Dispose();
+                        if(updateCount <= blockSize)
+                        {
+                            bulkInsertCmd.Append(cmd);
+                            bulkInsertCmd.Append(";");
+                            updateCount++;
+                        }
+                        else{
+                            pgsqlCommand.CommandText = bulkInsertCmd.ToString();
+                            pgsqlCommand.ExecuteNonQuery();
+                            pgsqlCommand.Dispose();
+                            updateCount = 0;
+                        }
                     }
                 }
             }
@@ -79,13 +108,76 @@ namespace TestLib
                 throw ex;
             }
 
-
-
-
             return "It worked!";        
         }
 
-        
+        public String TestUpdate2([MarshalAs(UnmanagedType.SafeArray)]ref string feedCode
+                                , [MarshalAs(UnmanagedType.SafeArray)]ref string feedValue)
+        {
+            try
+            {
+                WriteOnDB(feedCode, feedValue);
+                return "OK";
+            }
+            catch(Exception ex)
+            {
+                return "KO";
+            }
+        }
+
+        String WriteOnDB(String feedCode, String feedValue)
+        {
+            String hresult = String.Empty;
+            StringBuilder updateCommand = new StringBuilder();
+            String tableName = "data_double";
+
+            String thisFeedCode = String.Empty;
+            String thisFeedValue = String.Empty;
+
+            String connection = "Server=lx000000700501;Port=5432;User Id=quants;Password=quants;Database=quotes";
+            try
+            {
+                using (NpgsqlConnection pgsqlConnection = new NpgsqlConnection(connection))
+                {
+                    // Open the PgSQL Connection.                
+                    pgsqlConnection.Open();
+                    // Define command
+                    NpgsqlCommand pgsqlCommand = new NpgsqlCommand();
+                    pgsqlCommand.Connection = pgsqlConnection;
+
+                    thisFeedCode = feedCode;
+                    thisFeedValue = feedValue;
+
+                    updateCommand.Length = 0;
+                    updateCommand.Append("UPDATE ");
+                    updateCommand.Append(tableName);
+                    updateCommand.Append(" SET VALUE = ");
+                    updateCommand.Append(thisFeedValue);
+                    updateCommand.Append(" WHERE field_id = (SELECT id FROM fields WHERE field = 'VALUE') ");
+                    updateCommand.Append(" AND feedcode_id = (SELECT id FROM feedcodes WHERE label = '");
+                    updateCommand.Append(thisFeedCode);
+                    updateCommand.Append("')");
+
+                    string cmd = updateCommand.ToString();
+
+                    pgsqlCommand.CommandText = cmd;
+                    pgsqlCommand.ExecuteNonQuery();
+                    pgsqlCommand.Dispose();
+                }
+                hresult= "It worked!";
+            }
+            catch (NpgsqlException ex)
+            {
+                hresult = "It does not worked! ";
+            }
+            catch
+            {
+                hresult = "It does not worked!";
+            }
+            return hresult;
+        }
+
+
         // test methods ---------------------------------------------------------------
         public string[] textArray
         {
