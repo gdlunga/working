7dbfab3e6da4d4f6ec5e58db3f4e4d8d2182173d
gdlunga <giovanni.dellalunga@gmail.com>
2015-09-09 09:13:37 +0200

    dev

---
+++ R/statistica con R/scripts/common.r
@@ -0,0 +1,18 @@
+#_______________________________________________________________________________________________________________________________
+#
+# function			: working.dir 
+# description		: restituisce il path in cui si trova il folder di lavoro degli script R
+# last version	:
+# note				  :
+#
+working.dir <- function(){
+  require(stringr)
+  if(Sys.info()[1] == 'Windows'){
+    if(Sys.info()[4] == 'CL100400200004')
+      working_dir <- str_trim('D:/git/working/R/')
+    else
+      working_dir <- str_trim('C:/git/working/R/')
+  }else{
+    working_dir <- str_trim('/Users/giovanni/git_repository/working/R/')
+  }
+}
