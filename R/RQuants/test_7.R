09b1b125484b167c48235430a86b5cb037bdc97e
gdlunga <giovanni.dellalunga@gmail.com>
2016-03-23 18:25:40 +0100

    dev

--- R/RQuants/test_7.R
+++ R/RQuants/test_7.R
@@ -2,14 +2,17 @@ require(RQuantLib)
 #
 #-----------------------------------------------------------------------------------------------------------
 #
-FixedLeg <- function(params, schedule, calc, rates)
+FixedLeg <- function(params, schedule, rates)
 {
   thisEnv <- environment()
   
   me <- list(
     params = params,
     schedule = schedule,
-    calc = calc,
+    calc <- list(dayCounter='ActualActual', 
+                 compounding='Compounded',
+                 freq='Annual', 
+                 durationType='Modified'),
     rates = rates,
 
     price = function(discountCurve){
@@ -20,12 +23,12 @@ FixedLeg <- function(params, schedule, calc, rates)
       zcb_dateparams <-list(settlementDays=1,
                             calendar="UnitedStates/GovernmentBond",
                             businessDayConvention=4)
-      redemption <- ZeroCouponBond(zcb_params, discountCurve=discountCurve, zcb_dateparams)
       dummy<-FixedRateBond(params, 
                            rates, 
                            schedule, 
                            calc, 
                            discountCurve=discountCurve)
+      redemption <- ZeroCouponBond(zcb_params, discountCurve=discountCurve, zcb_dateparams)
       return (dummy[1]$NPV - redemption[1]$NPV)
     }
   )
@@ -45,13 +48,8 @@ FloatingLeg <- function(params, schedule)
     params = params,
     schedule = schedule,
 
-    price = function(discountCurve, liborCurve)
+    price = function(discountCurve, index)
     {
-      gearings <- spreads <- caps <- floors <- vector()
-      libor    <- list(type="USDLibor", 
-                       length=6, 
-                       inTermOf="Month",
-                       term=liborCurve)
       zcb_params <- list(faceAmount=100, 
                          issueDate=params$issueDate,
                          maturityDate=params$maturityDate, 
@@ -59,8 +57,15 @@ FloatingLeg <- function(params, schedule)
       zcb_dateparams <-list(settlementDays=1,
                             calendar="UnitedStates/GovernmentBond",
                             businessDayConvention=4)
+      
+      # invertendo l'ordine della chiamata non funziona. Capire perché!!!
+      dummy      <- FloatingRateBond(params, 
+                                     vector(), vector(), vector(), vector(),
+                                     index, 
+                                     curve=discountCurve, 
+                                     schedule)
       redemption <- ZeroCouponBond(zcb_params, discountCurve=discountCurve, zcb_dateparams)
-      dummy      <- FloatingRateBond(params, gearings, spreads, caps, floors,libor, discountCurve, schedule)
+      
       return (dummy[1]$NPV - redemption[1]$NPV)
     }
   )
@@ -82,11 +87,14 @@ SwapPV <- function(nominalExchange=FALSE, fixedLeg, floatingLeg)
     thisEnv = thisEnv,
     
     hasNominalExchange = nominalExchange,
-    fixedLeg = fixedLeg,
-    floatingLeg = floatingLeg,
+    fixedLeg           = fixedLeg,
+    floatingLeg        = floatingLeg,
     
-    price = function(discountCurve){
-      return (fixedLeg$price(discountCurve))
+    price = function(discountCurve, index){
+      
+      fl.price = floatingLeg$price(discountCurve, index)
+      fx.price = fixedLeg$price(discountCurve)
+      return (fx.price - fl.price)
     }
   )
   
@@ -100,40 +108,40 @@ SwapPV <- function(nominalExchange=FALSE, fixedLeg, floatingLeg)
 #
 #-----------------------------------------------------------------------------------------------------------
 #
+issueDate     <- as.Date("2014-01-15")
+maturityDate  <- as.Date("2024-01-15")
+swapRate      <- 0.02
+
 leg_1_params   <- list(settlementDays=1, 
-                       issueDate=as.Date("2014-01-15"),
+                       issueDate=issueDate,
                        faceAmount=100, 
                        accrualDayCounter='Thirty360',
                        paymentConvention='Unadjusted')
 
-leg_1_schedule <- list(effectiveDate=as.Date("2014-01-15"),
-                       maturityDate=as.Date("2024-01-15"),
-                       period='Semiannual',
-                       calendar='UnitedStates/GovernmentBond',
+leg_1_schedule <- list(effectiveDate=issueDate,
+                       maturityDate=maturityDate,
+                       period='Annual',
+                       calendar='TARGET',
                        businessDayConvention='Unadjusted',
                        terminationDateConvention='Unadjusted',
                        dateGeneration='Forward',
                        endOfMonth=1)
 
-leg_1_calc     <- list(dayCounter='Actual360', 
-                       compounding='Compounded',
-                       freq='Annual', 
-                       durationType='Modified')
 
-leg_1_rates    <- c(0.02)
+leg_1_rates    <- c(swapRate)
 #
 #-----------------------------------------------------------------------------------------------------------
 #
 leg_2_params   <- list(faceAmount=100, 
-                       issueDate=as.Date("2014-01-15"),
-                       maturityDate=as.Date("2024-01-15"), 
+                       issueDate=issueDate,
+                       maturityDate=maturityDate, 
                        redemption=100,
-                       effectiveDate=as.Date("2014-01-15"))
+                       effectiveDate=issueDate)
 
 leg_2_schedule <- list(settlementDays=1, 
-                       calendar="UnitedStates/GovernmentBond",
+                       calendar="TARGET",
                        dayCounter = 1, 
-                       period=3, 
+                       period=2, 
                        businessDayConvention = 1,
                        terminationDateConvention=1, 
                        dateGeneration=0, 
@@ -154,16 +162,16 @@ tsQuotes        <- list(flat=0.02)
 times           <- seq(0,10,.1)# Loglinear interpolation of discount factors
 discountCurve   <- DiscountCurve(params, tsQuotes, times)
 liborCurve      <- DiscountCurve(params,list(flat=0.05), times)
+libor           <- list(type="USDLibor", 
+                        length=6, 
+                        inTermOf="Month",
+                        term=liborCurve)
 #
 #-----------------------------------------------------------------------------------------------------------
 #
-fixedLeg        <- FixedLeg   (leg_1_params, leg_1_schedule, leg_1_calc, leg_1_rates)
+fixedLeg        <- FixedLeg   (leg_1_params, leg_1_schedule, leg_1_rates)
 floatingLeg     <- FloatingLeg(leg_2_params, leg_2_schedule)
 
 mySwap          <- SwapPV(FALSE, fixedLeg, floatingLeg)
 
-fl<-mySwap$floatingLeg
-fx<-mySwap$fixedLeg
-fl$price(discountCurve, liborCurve)
-
-
+mySwap$price(discountCurve, libor)
