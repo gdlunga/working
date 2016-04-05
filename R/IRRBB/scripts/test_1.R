c7cfd87d91c77fc1ad0bb11a6da13a74c5f8dbfe
gdlunga <giovanni.dellalunga@gmail.com>
2016-04-05 10:12:44 +0200

    dev

--- R/IRRBB/scripts/test_1.R
+++ R/IRRBB/scripts/test_1.R
@@ -1,5 +1,5 @@
 require(astsa)
-require(tseries)
+require(zoo)
 
 Dirname <- "/working/R/IRRBB/data"
 Dirs <- list.dirs(path=file.path("~"),recursive=T)
@@ -12,6 +12,31 @@ file_name = paste(getwd(),'hist_euribor.csv',sep='/')
 euribor <- read.csv(file_name, sep=';')
 euribor$Date = as.Date(euribor$Date)
 
-acf(euribor$X1.Month, 4000)
-adf.test(euribor$X1.Month)
+# serie giornaliere
+euribor1M = zoo(x=euribor$X1.Month, order.by=euribor$Date) 
+euribor6M = zoo(x=euribor$X6.Month, order.by=euribor$Date) 
+euribor1Y = zoo(x=euribor$X12.Month, order.by=euribor$Date) 
+
+
+euribor1M$yearmon = as.yearmon(euribor$Date,"%Y %b")
+
+
+plot(euribor1M)
+lines(euribor6M,col='red')
+lines(euribor1Y, col='blue')
+
+e1m = euribor$X1.Month
+
+
+w   = rnorm(length(e1m),0,1)
+alpha  = .2
+beta   = .25
+sigma  = 0.01
+z   = alpha + beta*e1m + sigma*w
+
+
+plot.ts(e1m)
+lines(z,col='red')
+
+y<-lm(z~e1m)
 
