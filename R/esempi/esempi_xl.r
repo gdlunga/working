595f0b801094cb53c320be36055f77ed4bba1809
gdlunga <giovanni.dellalunga@gmail.com>
2016-04-11 12:32:48 +0200

    dev

--- R/esempi/esempi_xl.r
+++ R/esempi/esempi_xl.r
@@ -9,7 +9,7 @@ require(binhf)
 working_dir = getwd()
 # import del file contenente i dati relativi all'indice S&P MIB
 file_name = paste(working_dir, 'ts_sp_mib.csv', sep="/")
-ts <- read.csv(file_name)
+ts <- read.csv(file_name, sep=";")
 ts$Date = as.Date(ts$Date)
 attach(ts)
 
