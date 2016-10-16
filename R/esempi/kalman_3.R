fe1087ac9a28f742f95a562500ce523600d77e09
Giovanni Della Lunga <giovanni.dellalunga@gmail.com>
2016-10-16 15:49:41 +0200

    dev

--- R/esempi/kalman_3.R
+++ R/esempi/kalman_3.R
@@ -1,16 +1,60 @@
-library(lattice)
-grid <- expand.grid(x=x1,y=x2)
-grid$Prior <- as.vector(z)
-grid$Likelihood <- as.vector(z2)
-grid$Posterior <- as.vector(z3)
-grid$Predictive <- as.vector(z4)
-contourplot(Prior + Likelihood + Posterior + Predictive ~ x*y, 
-            data=grid, col.regions=mycols, region=TRUE,
-            as.table=TRUE, 
-            xlab=expression(x[1]),
-            ylab=expression(x[2]),
-            main="Kalman Filter",
-            panel=function(x,y,...){
-              panel.grid(h=-1, v=-1)
-              panel.contourplot(x,y,...)
-            })
+library(KFAS)
+library(tseries)
+library(timeSeries)
+library(zoo)
+library(quantmod)
+
+
+getDailyPrices = function( tickerSym, startDate, endDate )
+{
+  prices = get.hist.quote( instrument = tickerSym, start = startDate, end = endDate,
+                           quote="AdjClose", provider="yahoo", 
+                           compression="d",  quiet=T)
+  
+  prices.ts = ts(prices)
+  return( prices.ts )
+}
+
+kalmanFilter = function( x )
+{
+  t = x
+  if (class(t) != "ts") {
+    t = ts(t)
+  }
+  ssModel = structSSM( y = t, distribution="Gaussian")
+  ssFit = fitSSM(inits=c(0.5*log(var(t)), 0.5*log(var(t))), model = ssModel )
+  kfs = KFS( ssFit$model, smoothing="state", nsim=length(t))
+  vals = kfs$a
+  lastVal = vals[ length(vals)]
+  return(lastVal)
+}
+
+Start = "2011-01-01"
+End   = "2012-12-31"
+SandP = "^GSPC"
+
+windowWidth = 20
+tsLength = 100
+
+SAndP.ts = getDailyPrices( SandP, Start, End )
+SAndP.ts = SAndP.ts[1:tsLength]
+SAndP.smoothed = rollapply( data=SAndP.ts, width=windowWidth, FUN=kalmanFilter)
+
+par(mfrow=c(1,1))
+prices = coredata( SAndP.ts[windowWidth:length(SAndP.ts)])
+plot(prices, col="blue", type="l")
+lines(coredata(SAndP.smoothed), col="magenta")
+par(mfrow=c(1,1))
+
+library(TTR)
+
+emaSmooth = function( x )
+{
+  ema = EMA(x)
+  val = ema[ length(ema) ]
+  return(val)
+}
+
+emaSmooth = rollapply( data = SAndP.ts, width=windowWidth, FUN=emaSmooth)
+lines(coredata(emaSmooth), col="magenta")
+      
