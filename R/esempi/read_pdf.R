dceeba9639ae00f2cce3d5758046f35e3285c02a
gdlunga <giovanni.dellalunga@gmail.com>
2016-11-23 19:33:12 +0100

    dev

--- R/esempi/read_pdf.R
+++ R/esempi/read_pdf.R
@@ -1,6 +1,32 @@
-#install.packages("tm") # only need to do once
-library(tm)
+#
+# Update R section ... just in case ... 
+#
+# installing/loading the package:
+#if(!require(installr)) {
+#  install.packages("installr"); require(installr)} #load / install+load installr
 
+# using the package:
+#updateR() # this will start the updating process of your R installation.  It will check for newer versions, and if one is available, will guide you through the decisions you'd need to make.
+
+
+#install.packages("tm") # only need to do once
+#install.packages("qdap")
+#install.packages("qdapDictionaries")
+#install.packages("dplyr")
+#install.packages("RColorBrewer")
+#install.packages("ggplot2")
+#install.packages("scales")
+#source("https://bioconductor.org/biocLite.R")
+#biocLite("Rgraphviz")
+
+library(tm)
+library(qdap)
+library(qdapDictionaries)
+library(dplyr)
+library(RColorBrewer)
+library(ggplot2)
+library(scales)
+library(Rgraphviz)
 
 file_path      = 'C:/Users/T004314/Documents/Data'
 setwd(file_path)
@@ -22,3 +48,11 @@ opinions.tdm <- TermDocumentMatrix(opinions, control = list(removePunctuation = 
 
 inspect(opinions.tdm[1:10,]) 
 findFreqTerms(opinions.tdm, lowfreq = 100, highfreq = Inf)
+
+
+
+setwd(script.dir <- dirname(sys.frame(1)$ofile))
+
+cname <- paste(getwd(),'corpus','pdf',sep='/')
+files = list.files(path = cname, pattern = "pdf$")
+files
