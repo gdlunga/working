89577468735a6876ec050a6434b659ec4b2b144b
gdlunga <giovanni.dellalunga@gmail.com>
2019-02-08 20:27:15 +0100

    dev

--- 01_R/appunti/2. text_mining/basic_text_mining_0.r
+++ 01_R/appunti/2. text_mining/basic_text_mining_0.r
@@ -1,26 +1,37 @@
-Needed <- c("tm", "SnowballCC", "RColorBrewer", "ggplot2", "wordcloud", "biclust", "cluster", "igraph", "fpc")   
-install.packages(Needed, dependencies=TRUE)   
-#install.packages("Rcampdf", repos = "http://datacube.wu.ac.at/", type = "source")    
+#Needed <- c("tm", "SnowballCC", "RColorBrewer", "ggplot2", "wordcloud", "biclust", "cluster", "igraph", "fpc")   
+#install.packages(Needed, dependencies=TRUE)   
 
-# On a PC, save the folder to your C: drive and use the following code chunk:
+library(tm)
+library(SnowballC)
+library(RColorBrewer)
+library(ggplot2)
+library(wordcloud)
+library(biclust)
+library(cluster)
+library(igraph)
+library(fpc)
 
-cname <- file.path("/Users/giovanni/Documents", "Corpus/txt")   
+setwd('C:\\Users\\T004314\\Documents\\GitHub\\working\\01_R\\appunti\\2. text_mining')
+
+# individuazione percorsi file dati
+data_path = 'C:\\Users\\T004314\\Documents\\Working\\2_MyTools'
+
+cname <- file.path(data_path, "txt")   
 cname   
 dir(cname)  
 
 # Load the R package for text mining and then load your texts into R
 
 library(tm)   
-docs <- Corpus(DirSource(cname))   
+docs <- VCorpus(DirSource(cname))   
 
 summary(docs)  
 
-write(docs[[1]]$content, file='prova.txt')
+#write(docs[[1]]$content, file='prova.txt')
 
 text1 <- docs[[1]]$content
-text2 <- docs[[2]]$content
-text3 <- docs[[3]]$content
-
+#text2 <- docs[[2]]$content
+#text3 <- docs[[3]]$content
 
 #inspect(docs[1])
 
@@ -54,18 +65,13 @@ for(j in seq(docs))
 docs <- tm_map(docs, stripWhitespace)  
 
 docs <- tm_map(docs, PlainTextDocument)   
-dtm <- DocumentTermMatrix(docs)   
+dtm  <- DocumentTermMatrix(docs)   
 dtm 
 
-
-
-
 # If you prefer to export the matrix to Excel:   
-
 m <- as.matrix(dtm)   
 dim(m)   
 write.csv(m, file="dtm.csv")   
-
 
 tdm  <- TermDocumentMatrix(docs)
 t <- as.matrix(tdm)
@@ -74,10 +80,10 @@ write.csv(t, file="tdm.csv")
 
 #  Start by removing sparse terms:   
 dtms <- removeSparseTerms(dtm, 0.1) # This makes a matrix that is 10% empty space, maximum.   
-inspect(dtms)  
+#inspect(dtms)  
 
 tdms <- removeSparseTerms(tdm, 0.1) # This makes a matrix that is 10% empty space, maximum.   
-inspect(tdms)  
+#inspect(tdms)  
 
 t <- as.matrix(tdms)
 write.csv(t, file="tdm.csv")   
@@ -118,6 +124,7 @@ plot(dtm, terms=c('unanimit?','votare','cfo','collegio','finanziamenti','dringol
 
 library(wordcloud) 
 
+
 #dev.new()
 set.seed(142)   
 wordcloud(names(freq), freq, min.freq=25)   
@@ -127,7 +134,7 @@ set.seed(142)
 wordcloud(names(freq), freq, min.freq=20, scale=c(5, .1), colors=brewer.pal(6, "Dark2"))   
 
 dtmss <- removeSparseTerms(dtm, 0.01) # This makes a matrix that is only 15% empty space, maximum.   
-inspect(dtmss)   
+#inspect(dtmss)   
 
 
 library(cluster)   
--- 01_R/appunti/2. text_mining/basic_text_mining_2.r
+++ 01_R/appunti/2. text_mining/basic_text_mining_2.r
@@ -5,12 +5,16 @@ if(!require(pdftools)){
   library(pdftools)
 } 
 
-# Creazione corpus txt
+# individuazione percorsi file dati
+data_path = 'C:\\Users\\T004314\\Documents\\Working\\2_MyTools'
+file_path_orig = file.path(data_path, 'pdf')
+file_path_dest = file.path(data_path, 'txt')
 
-file_path_orig = 'C:\\Users\\T004314\\Documents\\GitHub\\working\\01_R\\appunti\\2. text_mining\\pdf'
-file_path_dest = 'C:\\Users\\T004314\\Documents\\GitHub\\working\\01_R\\appunti\\2. text_mining\\txt'
-
-myfiles = list.files(file_path, pattern = "\\.pdf")
+# la funzione list.files permette di ottenere un vettore di stringhe
+# contenente i nomi dei file presenti del percorso passato in input
+# alla funzione con un pattern di ricerca definito dall'utente 
+# (nel nostro caso pdf)
+myfiles = list.files(file_path_orig, pattern = "\\.pdf")
 
 
 for(thisfile in myfiles){
@@ -18,9 +22,11 @@ for(thisfile in myfiles){
   # da pdf. Nota. Capire bene perché per sottrarre gli ultimi 4 caratteri
   # occorre passare il valore 5!
   txt_file_name = paste(str_sub(thisfile,1,length(thisfile)-5),"txt",sep="")  
+  # aggiunta del path di destinazione
   txt_file_name = file.path(file_path_dest, txt_file_name)
+  # conversione dei pdf in txt
   txt <- pdf_text(file.path(file_path_orig,thisfile))  
-
+  # apertura file di testo e scrittura del testo estratto dal file pdf
   fileConn = file(txt_file_name)
   write(txt, fileConn)
   close(fileConn)
