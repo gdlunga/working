ac3098159675e0865ea6dad0c74f5dce60cc5092
gdlunga <giovanni.dellalunga@gmail.com>
2020-01-09 21:52:02 +0100

    dev

--- 04_Other/java/Ch02_Example_03/structured/Bin_Cash_or_Nothing.java
+++
@@ -1,6 +0,0 @@
-package structured;
-
-
-public class Bin_Cash_or_Nothing {
-
-}
--- 04_Other/java/Ch04_JExcalibur/jexcalibur/scheduler/IInterval.java
+++
@@ -1,9 +0,0 @@
-package jexcalibur.scheduler;
-
-
-public interface IInterval {
-
-	public abstract int 	period();
-	public abstract int 	periodMultiplier();
-	public abstract String 	periodCode();
-}
